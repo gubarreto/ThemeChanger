d338f49716e9ad379f029a4108b077e93f284e44
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-12-04 09:11:09 -0300

    Initial Commit

---
+++ ThemeChanger/ThemeChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemeChangerApp.swift
+//  ThemeChanger
+//
+//  Created by Gustavo on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ThemeChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
