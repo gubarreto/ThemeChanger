eb9074771bb5cf26fa650da453b9a02bab9e5e00
Gustavo Barreto <gustavobarretodaf@gmail.com>
2024-12-04 10:35:05 -0300

    :sparkles: feat: Created changeTheme sheet

--- ThemeChanger/ContentView.swift
+++ ThemeChanger/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// View Properties
+    @State private var changeTheme: Bool = false
+    @Environment(\.colorScheme) private var scheme
+    @AppStorage("userTheme") private var userTheme: Theme = .systemDefault
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("Appearance") {
+                    Button("Change Theme") {
+                        changeTheme.toggle()
+                    }
+                }
+            }
+            .navigationTitle("Settings")
         }
-        .padding()
+        .preferredColorScheme(userTheme.colorScheme)
+        .sheet(isPresented: $changeTheme, content: {
+            ThemeChangeView(scheme: scheme)
+            /// Since Max Height is 410
+                .presentationDetents([.height(410)])
+                .presentationBackground(.clear)
+        })
     }
 }
 
